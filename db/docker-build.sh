d32856d2232e2117b0e2bf2a4cb8fe9a3f358411
Ville Peurala <ville.peurala@gmail.com>
2017-10-17 20:10:44 +0300

    Successfully mount a volume.

--- db/docker-build.sh
+++ db/docker-build.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
 docker build --tag vpeurala/purescript-possu-db:latest .
-docker rm purescript-possu-db-latest
-docker run --publish 5432:5432 --env POSTGRES_PASSWORD=leipuri --detach vpeurala/purescript-possu-db:latest
+docker run --detach --env POSTGRES_PASSWORD=leipuri --publish 5432:5432 --volume ~/.docker/possu/data:/var/lib/postgresql/data vpeurala/purescript-possu-db:latest
 
