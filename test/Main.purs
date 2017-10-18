7cbab3568fd89c25654efc04529314493cddb705
Ville Peurala <ville.peurala@gmail.com>
2017-10-18 14:49:45 +0300

    Spawn a child process, then kill it.

--- test/Main.purs
+++ test/Main.purs
@@ -3,12 +3,16 @@ module Test.Main where
 import Node.ChildProcess (ChildProcess, CHILD_PROCESS)
 import Node.ChildProcess as ChildProcess
 
+import Data.Posix.Signal (Signal(..))
+
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
 import Prelude
 
-main :: forall e. Eff (child_process :: CHILD_PROCESS, console :: CONSOLE | e) Unit
+main :: forall e. Eff (cp :: CHILD_PROCESS, console :: CONSOLE | e) Unit
 main = do
   dockerDbProcess <- ChildProcess.spawn "db/docker-build.sh" [] ChildProcess.defaultSpawnOptions
   log "Spawned"
+  killResult <- ChildProcess.kill SIGTERM dockerDbProcess
+  log $ "Killed: " <> (show killResult)
