1d8a946ba0b2bb1f857e8cfabf719b792de40afc
Ville Peurala <ville.peurala@gmail.com>
2017-10-18 18:41:22 +0300

    Convert test to purescript-spec format.

--- test/Main.purs
+++ test/Main.purs
@@ -2,19 +2,27 @@ module Test.Main where
 
 import Node.ChildProcess (CHILD_PROCESS, defaultSpawnOptions, kill, pid, spawn)
 
-import Data.Posix.Signal (Signal(..))
+import Data.Posix.Signal (Signal(SIGTERM))
 
 import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.AVar (AVAR)
+import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+
+import Test.Spec (describe, it)
+import Test.Spec.Reporter.Console (consoleReporter)
+import Test.Spec.Runner (PROCESS, run)
 
 import Prelude
 
-main :: forall e. Eff (cp :: CHILD_PROCESS, console :: CONSOLE | e) Unit
-main = do
-  dockerDbProcess <- spawn "db/docker-build.sh" [] defaultSpawnOptions
-  log $ "Spawned: " <> (show $ pid dockerDbProcess)
-  doTestStuff
-  killResult <- kill SIGTERM dockerDbProcess
-  log $ "Killed: " <> (show killResult)
+main :: forall eff. Eff (console :: CONSOLE, avar :: AVAR, process :: PROCESS, cp :: CHILD_PROCESS | eff) Unit
+main = run [consoleReporter] do
+  describe "FFI spike" do
+    it "does not crash" do
+      dockerDbProcess <- liftEff $ spawn "db/docker-build.sh" [] defaultSpawnOptions
+      liftEff $ log $ "Spawned: " <> (show $ pid dockerDbProcess)
+      liftEff doTestStuff
+      killResult <- liftEff $ kill SIGTERM dockerDbProcess
+      liftEff $ log $ "Killed: " <> (show killResult)
 
 foreign import doTestStuff :: forall e. Eff (e) Unit
