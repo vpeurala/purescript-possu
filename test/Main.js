aada5415ea4deb38d54b6389f25eded982f624ce
Ville Peurala <ville.peurala@gmail.com>
2017-10-18 21:14:15 +0300

    ESLint rules.

--- test/Main.js
+++ test/Main.js
@@ -1,21 +1,37 @@
 'use strict';
 
-const pg = require('pg');
-const Promise = require('bluebird');
+var pg = require('pg');
+var Promise = require('bluebird');
 
 function doTestStuff() {
     console.log('Doing test stuff.');
-    const pool = new pg.Pool({
+    var pool = new pg.Pool({
         user: 'postgres',
         host: 'localhost',
         database: 'postgres',
         password: 'leipuri',
-        port: 5432
+        port: 5432,
+        Promise: Promise
     });
-    pool.query('SELECT name FROM viha', function (err, res) {
-        console.log(err, res);
-        pool.end();
+    pool.on('connect', function (client) {
+        console.log("pool.on('connect')", client);
     });
+    pool.on('acquire', function (client) {
+        console.log("pool.on('acquire')", client);
+    });
+    pool.on('error', function (err, client) {
+        console.log("pool.on('error')", err, client);
+    });
+    console.log("Before query");
+    pool.query('SELECT name FROM viha')
+        .then(function (res) {
+            console.error(res);
+        })
+        .catch(function (err) {
+            console.error("Error executing query", err.stack);
+        });
+    pool.end();
+    console.log("After query");
 }
 
 exports.doTestStuff = doTestStuff;
