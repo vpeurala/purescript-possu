fb7059d0bac74a11837f2f9d39c1a3838fe0d839
Ville Peurala <ville.peurala@gmail.com>
2017-10-19 22:35:14 +0300

    Extract setUpDb and tearDownDb.

--- test/Main.purs
+++ test/Main.purs
@@ -1,6 +1,6 @@
 module Test.Main where
 
-import Node.ChildProcess (CHILD_PROCESS, defaultSpawnOptions, kill, pid, spawn)
+import Node.ChildProcess (CHILD_PROCESS, ChildProcess, defaultSpawnOptions, kill, pid, spawn)
 
 import Data.Posix.Signal (Signal(SIGTERM))
 
@@ -8,7 +8,7 @@ import Control.Monad.Aff (Aff)
 import Control.Monad.Aff.Console (CONSOLE, log)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.AVar (AVAR)
-import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Class (class MonadEff, liftEff)
 
 import Test.Spec (describe, it)
 import Test.Spec.Reporter.Console (consoleReporter)
@@ -20,10 +20,16 @@ main :: forall eff. Eff (console :: CONSOLE, avar :: AVAR, process :: PROCESS, c
 main = run [consoleReporter] do
   describe "FFI spike" do
     it "does not crash" do
-      dockerDbProcess <- liftEff $ spawn "db/docker-build.sh" [] defaultSpawnOptions
+      dockerDbProcess <- setUpDb
       log $ "Spawned: " <> (show $ pid dockerDbProcess)
       liftEff doTestStuff
-      killResult <- liftEff $ kill SIGTERM dockerDbProcess
+      killResult <- liftEff $ tearDownDb dockerDbProcess
       log $ "Killed: " <> (show killResult)
+
+setUpDb :: forall eff ret. MonadEff (cp :: CHILD_PROCESS | eff) ret => ret ChildProcess
+setUpDb = liftEff $ spawn "db/docker-build.sh" [] defaultSpawnOptions
+
+tearDownDb :: forall eff ret. MonadEff (cp :: CHILD_PROCESS | eff) ret => ChildProcess -> ret Boolean
+tearDownDb dbProcess = liftEff $ kill SIGTERM dbProcess
 
 foreign import doTestStuff :: forall e. Eff (e) Unit
