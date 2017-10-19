1aef894152f4ba1b6990a8fc4f0d10fd865795ae
Ville Peurala <ville.peurala@gmail.com>
2017-10-19 19:49:46 +0300

    WIP. Try all kinds of stuff, decided to drop bluebird at first.

--- test/Main.js
+++ test/Main.js
@@ -1,32 +1,34 @@
 'use strict';
 
 var Promise = require('bluebird');
-var pg_pool = require('pg-pool');
-var Pool = Promise.promisifyAll(pg_pool.Pool);
+var pg = require('pg');
+var Pool = Promise.promisifyAll(pg.Pool);
+
+console.log('Pool: ', Pool);
 
 process.on('unhandledRejection', function (reason, promise) {
-    console.error(reason, promise);
+    console.error('unhandledRejection', reason, promise);
 });
 process.on('rejectionHandled', function (promise) {
-    console.error(promise);
+    console.error('rejectionHandled', promise);
 });
 process.on('promiseCreated', function (promise, child) {
-    console.error(promise, child);
+    console.error('promiseCreated', promise, child);
 });
 process.on('promiseChained', function (promise, child) {
-    console.error(promise, child);
+    console.error('promiseChained', promise, child);
 });
 process.on('promiseFulfilled', function (promise, child) {
-    console.error(promise, child);
+    console.error('promiseFulfilled', promise, child);
 });
 process.on('promiseRejected', function (promise, child) {
-    console.error(promise, child);
+    console.error('promiseRejected', promise, child);
 });
 process.on('promiseResolved', function (promise, child) {
-    console.error(promise, child);
+    console.error('promiseResolved', promise, child);
 });
 process.on('promiseCancelled', function (promise, child) {
-    console.error(promise, child);
+    console.error('promiseCancelled', promise, child);
 });
 
 function doTestStuff() {
@@ -36,9 +38,9 @@ function doTestStuff() {
         host: 'localhost',
         database: 'postgres',
         password: 'leipuri',
-        port: 5432,
-        Promise: Promise
+        port: 5432
     });
+    console.log('pool: ', pool);
     pool.on('connect', function (client) {
         console.log('pool.on(\'connect\')', client);
     });
@@ -49,15 +51,22 @@ function doTestStuff() {
         console.log('pool.on(\'error\')', err, client);
     });
     console.log('Before query');
-    pool.queryAsync('SELECT name FROM viha')
+    var testPromise = pool.queryAsync('SELECT name FROM viha')
         .then(function (res) {
             console.log('Response: ', res);
         })
         .catch(function (err) {
             console.error('Error executing query', err.stack);
+        })
+        .finally(function () {
+            pool.end();
         });
-    pool.end();
     console.log('After query');
+    setInterval(function () {
+        if (!testPromise.isPending()) {
+            return testPromise.value;
+        }
+    }, 1000);
 }
 
 exports.doTestStuff = doTestStuff;
