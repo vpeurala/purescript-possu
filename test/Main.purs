76d3506247d8e674a09e1f1d61b84445ec7e9deb
Ville Peurala <ville.peurala@gmail.com>
2017-10-20 20:08:07 +0300

    WIP. A temporary commit at the end of the day. Does not compile.

--- test/Main.purs
+++ test/Main.purs
@@ -2,15 +2,23 @@ module Test.Main where
 
 import Node.ChildProcess (CHILD_PROCESS, ChildProcess, defaultSpawnOptions, kill, pid, spawn)
 
-import Data.Foreign (Foreign)
+import Data.Either (Either, either)
+import Data.Foreign (Foreign, MultipleErrors, tagOf, typeOf, unsafeReadTagged)
+import Data.Foreign.Class (class Decode, decode)
+import Data.Foreign.Keys (keys)
 import Data.Posix.Signal (Signal(SIGTERM))
+import Data.Traversable (sequence)
 
+import Control.Monad.Aff (Aff)
 import Control.Monad.Aff.Compat (EffFnAff, fromEffFnAff)
 import Control.Monad.Aff.Console (CONSOLE, log)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.AVar (AVAR)
 import Control.Monad.Eff.Class (class MonadEff, liftEff)
-import Control.Monad.Eff.Exception (try)
+import Control.Monad.Eff.Exception (Error, error, try)
+import Control.Monad.Error.Class (throwError)
+import Control.Monad.Except (runExcept)
+--import Control.Monad.Except.Trans (ExpectT(..), runExpectT)
 
 --import Data.Generic (class Generic, gShow)
 
@@ -26,8 +34,9 @@ main = run [consoleReporter] do
     it "does not crash" do
       dockerDbProcess <- setUpDb
       log $ "Spawned: " <> (show $ pid dockerDbProcess)
-      doTestStuffResult <- liftEff $ try doTestStuff
-      log $ "doTestStuffResult: " <> (show doTestStuffResult)
+      rows <- fromEffFnAff doTestStuff
+      x <- either liftError pure (runExcept (sequence $ decode <$> rows))
+      log $ "doTestStuffResult: " <> (show x)
       killResult <- liftEff $ tearDownDb dockerDbProcess
       log $ "Killed: " <> (show killResult)
 
@@ -37,7 +46,7 @@ setUpDb = liftEff $ spawn "db/docker-build.sh" [] defaultSpawnOptions
 tearDownDb :: forall eff ret. MonadEff (cp :: CHILD_PROCESS | eff) ret => ChildProcess -> ret Boolean
 tearDownDb dbProcess = liftEff $ kill SIGTERM dbProcess
 
-foreign import doTestStuff :: forall eff. Eff (eff) Unit
+foreign import doTestStuff :: forall eff. EffFnAff (eff) (Array Foreign)
 
 {--
 derive instance genericForeign :: Generic Foreign
@@ -45,3 +54,6 @@ derive instance genericForeign :: Generic Foreign
 instance showForeign :: Show Foreign where
   show = gShow
 --}
+
+liftError :: forall e a. MultipleErrors -> Aff e a
+liftError errs = throwError $ error (show errs)
